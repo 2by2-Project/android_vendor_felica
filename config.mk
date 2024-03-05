85851db8bc0a5143f3e9db992ed437fab967655a
lahaina <nattolecats@gmail.com>
2024-03-05 23:34:59 +0900

    felica: Change LOCAL_PATH to FELICA_PATH * LOCAL_PATH not working

--- config.mk
+++ config.mk
@@ -3,13 +3,13 @@
 #  SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := $(call my-dir)
+FELICA_PATH := vendor/felica
 
 TARGET_SUPPORTS_FELICA := true
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH) \
+    $(FELICA_PATH) \
     vendor/nxp/opensource/commonsys/packages/apps/Nfc \
     vendor/nxp/opensource/commonsys/external/libnfc-nci \
     vendor/nxp/opensource/interfaces \
@@ -36,8 +36,8 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(LOCAL_PATH)/vintf/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(LOCAL_PATH)/vintf/device_framework_matrix.xml
+DEVICE_MANIFEST_FILE += $(FELICA_PATH)/vintf/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(FELICA_PATH)/vintf/device_framework_matrix.xml
 
 # Get non-open-source specific aspects.
 $(call inherit-product, vendor/felica/prebuilts/prebuilts-vendor.mk)
